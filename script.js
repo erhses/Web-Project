145bf59dbee517ea0449005880d13dc93d129626
erhses <apinsanity257@gmail.com>
2023-11-01 10:01:53 +0800

    [11.1 lab]

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let cart = [];
 document.addEventListener('DOMContentLoaded', function() {
     function toggleNav() {
       var navItems = document.querySelector('.nav-items');
-      navItems.classList.toggle('active');
+      navItems.classList.toggle('active');  
     }
   
     document.querySelector('.hamburger-menu').addEventListener('click', toggleNav);
