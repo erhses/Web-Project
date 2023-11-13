9c1bbd8153bde847790f6ec219be49a5c5f4a532
erhses <apinsanity257@gmail.com>
2023-11-14 01:16:50 +0800

    [fix] navbar responsivness

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,15 @@
-var mainListDiv = document.getElementById("mainListDiv"),
-mediaButton = document.getElementById("mediaButton");
+var mainList = document.getElementById("mainList"),
+  mediaButton = document.getElementById("media"),
+  navigationImage = document.querySelector(".navigation__img")
 
 mediaButton.onclick = function () {
+  "use strict";
 
-"use strict";
-
-mainListDiv.classList.toggle("show_list");
-mediaButton.classList.toggle("active");
-
+  mainList.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
+  if (navigationImage.style.display === "none") {
+    navigationImage.style.display = "block";
+} else {
+    navigationImage.style.display = "none";
+}
 };
