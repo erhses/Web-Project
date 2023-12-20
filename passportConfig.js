4e5de23db9e9b536e399c8d4f3c7944f083b8524
erhses <apinsanity257@gmail.com>
2023-12-20 21:37:12 +0800

    login hiiheeer redirect hiiihgueeennn aaaaa

--- passportConfig.js
+++ passportConfig.js
@@ -1,35 +1,51 @@
 const LocalStrategy = require("passport-local").Strategy;
-const { authenticate } = require("passport");
+const { authenticate, initialize } = require("passport");
 const {pool} = require("./dbConfig");
 const bcrypt = require("bcrypt");
 
-function init(passport){
+function configurePassport(passport){
     const authenticateUser = (email, password, done)=>{
         pool.query(
-            `SELECT`
-        )
-    }
-    passport.use(new LocalStrategy({
+            `SELECT * FROM users WHERE email = $1`, [email], (err, results) => {
+                if(err){
+                    throw err;
+                }
+                console.log(results.rows);
+                if(results.rows.length > 0){
+                    const user = results.rows[0];
+                    bcrypt.compare(password, user.password, (err, isMatch)=>{
+                        if(err){
+                            throw err;
+                        }
+                        if(isMatch){
+                            return done(null, user);
+                        }else{
+                            return done(null, false, {message: "password incorrect"});  
+                        }
+                    });
+                }else{
+                    return done(null, false, {message: "Not registered"});
+             }
+            }
+        );
+    };
+    passport.use(new LocalStrategy(
+        {
         usernameField : "email",
         passwordField : "password"
-    }, authenticate)
+    }, authenticateUser)
     );
-}
 
-if(results.rows.length > 0){
-    const user = results.rows[0];
-
-    bcrypt.compare(password, user.password, (err, isMatch) => {
-        if(err) {
-            throw err;
-        }
-
-        if(isMatch){
-            return done(null, user);
-        } else{
-            return done(null, false, {message: "password incorrect"});
-        }
+    passport.serializeUser((user, done)=>done(null, user.id));
+    passport.deserializeUser((id, done)=>{
+        pool.query(
+            `SELECT * FROM users WHERE id = $1`, [id], (err, results)=>{
+                if(err){
+                    throw err;
+                }
+                return done(done, results.rows[0]);
+            });
     });
-}else{
-    return done(null, false, {message: "Not registered"});
-}
+};
+
+module.exports = configurePassport;
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
-const {pool} = require("./dbConfig.js");
+const {pool} = require('./dbConfig');
 const bcrypt = require("bcrypt");
-const { ContextExclusionPlugin } = require('webpack');
+const { ContextExclusionPlugin } = require("webpack");
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");   
 
+
+const initializePassport = require("./passportConfig");
+initializePassport(passport);
 const PORT  = process.env.PORT || 5000;
 
 app.use('/app', express.static('app'));
 app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: "secret", 
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(flash());
 
 app.get('/', (req, res) => {
     res.sendFile('app/index.html', {root: __dirname })
@@ -18,14 +34,21 @@ app.get('/map', (req, res) => {
 app.get('/services', (req, res) => {
     res.sendFile('app/services.html', {root: __dirname })
 });
-app.get('/login', (req, res) => {
+app.get('/login', checkAuthenticated, (req, res) => {
     res.sendFile('app/signIn.html', {root: __dirname })
 });
-app.get('/profile', (req, res) => {
+app.get('/profile', checkNotAuthenticated, (req, res) => {
     res.sendFile('app/profile.html', {root: __dirname })
 });
-app.get('/register', (req, res) => {
+app.get('/register', checkAuthenticated, (req, res) => {
     res.sendFile('app/signUp.html', {root: __dirname })
+});
+app.get('/logout', (req,res) => {
+    req.logOut(function(err) {
+        if (err) { return next(err); }
+        req.flash("success_msg", "You have successfully logged out.");
+        res.redirect("/login");
+    });
 });
 
 
@@ -57,10 +80,46 @@ app.post('/register', async (req, res) => {
                     // res.send({
                     //     message: "Email already exists"
                     // });
+                }else{
+                    pool.query(
+                        `INSERT INTO users (name, email, password)
+                        VALUES ($1, $2, $3)
+                        RETURNING id, password`, [name, email, hashed], (err, result) =>{
+                                if(err){
+                                    throw err;
+                                }
+                                console.log(result.rows);
+                                req.flash('success_msg', "Registred!");
+                                res.redirect('/login');
+                        }
+                    )
                 }
             }
         );
 });
+
+app.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/profile",
+      failureRedirect: "/login",
+      failureFlash: true
+    })
+  );
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/profile");
+    }
+    next();
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/login");
+  }
 
 app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`);
