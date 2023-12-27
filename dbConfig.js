6d8590c16b30efcfb3c0ba6088873c45beab1b29
erhses <apinsanity257@gmail.com>
2023-12-27 16:32:03 +0800

    [fix] DB hosting

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://erhses:passwd@localhost:5432/nodelogin`;
+const connectionString = `postgres://rexlpzst:Vn7zU32D3K29GUpz3HR2crYWLlMRMdg-@satao.db.elephantsql.com/rexlpzst`;
 
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
