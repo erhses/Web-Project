555291ab252332dcb29ce78e3fc5d5e55f212f11
erhses <apinsanity257@gmail.com>
2024-01-02 18:15:52 +0800

    [add] map routing with multiple locations

--- app/assets/scripts/modules/maps.js
+++ app/assets/scripts/modules/maps.js
@@ -1,19 +1,37 @@
 function initMap() {
-    var map = L.map('map').setView([37.7749, -122.4194], 10);
+    var map = L.map('map');
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
-    var selectedItems = JSON.parse(localStorage.getItem('selectedItems'));
+    addMarkersAndDrawRoute(map);
+}
 
-    if (selectedItems && selectedItems.length >= 2) {
-        var startWaypoint = L.latLng(selectedItems[0].long, selectedItems[0].lat);
-        var endWaypoint = L.latLng(selectedItems[1].long, selectedItems[1].lat);
+function addMarkersAndDrawRoute(map) {
+    var selectedItems = JSON.parse(localStorage.getItem('selectedItems')) || [];
+
+    if (selectedItems.length >= 2) {
+        var routeLatLngs = selectedItems.map(function (item) {
+            return [item.long, item.lat];
+        });
+
+        var bounds = L.latLngBounds(routeLatLngs);
+
+        routeLatLngs.forEach(function (latLng, index) {
+            L.marker(latLng).addTo(map)
+                .bindPopup('<b>' + selectedItems[index].title + '</b>');
+        });
+
+        map.fitBounds(bounds);
+
+        var waypoints = routeLatLngs.map(function (latLng) {
+            return L.latLng(latLng[0], latLng[1]);
+        });
 
         L.Routing.control({
-            waypoints: [startWaypoint, endWaypoint],
+            waypoints: waypoints,
             routeWhileDragging: true,
             show: true,
-            router: L.Routing.mapbox('pk.eyJ1IjoiZXJoc2VzIiwiYSI6ImNscXZ4cXgxdTUxd3oya280YjQ5ZDU4NTMifQ.BypQD2x71oOXPLpGmWp7ew'), // Replace with your actual Mapbox Access Token
+            router: L.Routing.mapbox('pk.eyJ1IjoiZXJoc2VzIiwiYSI6ImNscXZ4cXgxdTUxd3oya280YjQ5ZDU4NTMifQ.BypQD2x71oOXPLpGmWp7ew'), //api key for mapbox
             createMarker: function (i, waypoint, n) {
                 return L.marker(waypoint.latLng, {
                     draggable: true,
