2aa8d8f94ed09b651737e4706276c439d2647ef4
erhses <apinsanity257@gmail.com>
2024-01-02 13:59:59 +0800

    [add]: route api

--- app/assets/scripts/modules/maps.js
+++ app/assets/scripts/modules/maps.js
@@ -1,41 +1,36 @@
 function initMap() {
-    var map = L.map('map').setView([48.8566, 2.3522], 12);
+    var map = L.map('map').setView([37.7749, -122.4194], 10);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-    AddMarkersAndDrawRoute(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    var selectedItems = JSON.parse(localStorage.getItem('selectedItems'));
+
+    if (selectedItems && selectedItems.length >= 2) {
+        var startWaypoint = L.latLng(selectedItems[0].long, selectedItems[0].lat);
+        var endWaypoint = L.latLng(selectedItems[1].long, selectedItems[1].lat);
+
+        L.Routing.control({
+            waypoints: [startWaypoint, endWaypoint],
+            routeWhileDragging: true,
+            show: true,
+            router: L.Routing.mapbox('pk.eyJ1IjoiZXJoc2VzIiwiYSI6ImNscXZ4cXgxdTUxd3oya280YjQ5ZDU4NTMifQ.BypQD2x71oOXPLpGmWp7ew'), // Replace with your actual Mapbox Access Token
+            createMarker: function (i, waypoint, n) {
+                return L.marker(waypoint.latLng, {
+                    draggable: true,
+                    icon: L.divIcon({ className: 'leaflet-div-icon', html: '<span>' + (i + 1) + '</span>' })
+                });
+            }
+        })
+        .on('routesfound', function(e) {
+            console.log('Routes found:', e.routes);
+        })
+        .on('routingerror', function(e) {
+            console.error('Routing error:', e.error);
+        })
+        .addTo(map);
+    }
 }
 
-function AddMarkersAndDrawRoute(map) {
-    L.marker([48.858844, 2.294351]).addTo(map)
-        .bindPopup('<b>Eiffel Tower</b>');
-
-    L.marker([48.860611, 2.337644]).addTo(map)
-        .bindPopup('<b>Louvre Museum</b>');
-
-    DrawRoute(map, [
-        [48.858844, 2.294351], // Eiffel Tower
-        [48.860611, 2.337644]  // Louvre Museum
-    ]);
-}
-
-// DrawRoute function
-function DrawRoute(map, routeLatLngs) {
-    L.Routing.control({
-        waypoints: routeLatLngs.map(function (latLng) {
-            return L.latLng(latLng[0], latLng[1]);
-        }),
-        routeWhileDragging: true,
-        show: true,
-        createMarker: function (i, waypoint, n) {
-            return L.marker(waypoint.latLng, {
-                draggable: true,
-                icon: L.divIcon({ className: 'leaflet-div-icon', html: '<span>' + (i + 1) + '</span>' })
-            });
-        }
-    }).addTo(map);
-}
 document.addEventListener('DOMContentLoaded', function () {
     initMap();
 });
